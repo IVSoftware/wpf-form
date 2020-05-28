1eefe11f79b45aea629b7496c1c2d7f2f0fbec21
IVSoftware <tgregor@ivsoftware.com>
2020-05-28 17:21:19 -0600

    Add project files.

---
+++ wpf-form/Form.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace IVSoftware
+{
+    public partial class Form : System.Windows.Window
+    {
+        public Form()
+        {
+            Loaded += (object sender, System.Windows.RoutedEventArgs e)=> { };
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+        }
+        protected virtual void OnLoaded(System.Windows.RoutedEventArgs e) {  }
+        public event EventHandler HandleCreated;
+        protected virtual void OnHandleCreated (System.Windows.RoutedEventArgs e) 
+        {
+            HandleCreated?.Invoke(this, e);
+        }
+    }
+}
